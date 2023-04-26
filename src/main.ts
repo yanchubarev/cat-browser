f2237e3276a7ef1a499c21fe19d6b41c61b8cb88
yanchubarev <yanchubarev@gmail.com>
2023-04-26 15:19:57 +0200

    cat-browser: remove unused imports

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,12 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import PrimeVue from "primevue/config";
-import "primevue/resources/themes/lara-light-indigo/theme.css";
-import "primevue/resources/primevue.min.css";
 import VueToast from "vue-toastification";
 import "vue-toastification/dist/index.css";
 
 const app = createApp(App);
 
 app.use(router);
-app.use(PrimeVue);
 app.use(VueToast);
 
 app.mount("#app");

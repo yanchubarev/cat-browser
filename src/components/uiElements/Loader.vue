e23551a86da3b20fbc26c4f9a85f291790092dc8
yanchubarev <yanchubarev@gmail.com>
2023-04-26 17:58:27 +0200

    cat-browser: Implement loader

---
+++ src/components/uiElements/Loader.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="spinner-grow text-primary" role="status">
+    <span class="visually-hidden">Loading...</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "LoaderComponent",
+});
+</script>

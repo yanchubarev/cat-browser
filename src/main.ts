6d7460b472ef72d0482c48323280a6c9c8153e61
yanchubarev <yanchubarev@gmail.com>
2023-04-26 14:12:45 +0200

    cat-browser: remove comments

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import PrimeVue from "primevue/config";
-//theme
 import "primevue/resources/themes/lara-light-indigo/theme.css";
-
-//core
 import "primevue/resources/primevue.min.css";
-
 import VueToast from "vue-toastification";
 import "vue-toastification/dist/index.css";
 
